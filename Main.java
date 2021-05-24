d7d45f0f34ef01a4af8785b3a93dbf2b372333ba
Maciej Krok <inz.krok.maciej@gmail.com>
2021-05-24 16:21:37 +0200

    Main class modify after adding pseudo code generator

--- Main.java
+++ Main.java
@@ -1,10 +1,29 @@
 package bullscows;
 
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Grader.grader();
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        if (n > 10) {
+            System.out.println("Error: can't generate a secret number with a length of" +
+                    n + "because there aren't enough unique digits.");
+        } else {
+            String code = GeneratedCode.codeGenerator(n);
+
+            while (code.length() < n) {
+                code = GeneratedCode.codeGenerator(n);
+            }
+
+            System.out.println("The random secret number is " + code + ".");
+        }
+
+       // Grader.grader();
 
 
 
